bddaca6094d10c7c2273b2b00cd7eaff40029daa
Alexander Roikier Møller <alex6308@elev.tec.dk>
2018-08-30 13:02:51 +0200

    <Overhovedt ikke færdig>

---
+++ OOP Figurer/OOP Figurer/Figur.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Figurer
+{
+    abstract class Figur
+    {
+        public double Size;
+
+        public double GetAreal(double Size)
+        {
+            double areal = Size * Size;
+            return areal;
+        }
+    }
+}
---
+++ OOP Figurer/OOP Figurer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Figurer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kvadrat k1 = new Kvadrat { Size = 40 };
+            Cirkel c1 = new Cirkel { Size = 20 };
+
+            k1.GetAreal();
+
+            Console.ReadKey();
+        }
+    }
+}
